1c7ee21882aa7d613cea923b7271caf5aebd843f
Annie <overheads165@gmail.com>
2022-12-26 22:50:30 +0100

    post request

--- src/server.js
+++ src/server.js
@@ -1,52 +1,62 @@
 const fs = require("fs");
 const http = require("http");
 const Koa = require("koa");
-const koaBody = require("koa-body");
+const { koaBody } = require("koa-body");
 const koaStatic = require("koa-static");
 const path = require("path");
 const uuid = require("uuid");
 
-
-const allTickets = [{
+const allTickets = [
+  {
     id: 1,
     name: "Ticket 1",
     description: "Description 1",
     status: "open",
     created: "2020-01-01",
-},
-{
+  },
+  {
     id: 2,
     name: "Ticket 2",
     description: "Description 2",
     status: "open",
     created: "2020-01-01",
-}]
+  },
+];
 
 const app = new Koa();
+app.use(koaBody());
 
-// logger
+// Logger
 app.use(async (ctx, next) => {
   await next();
-  console.log(
-    `Method: ${ctx.method}  URL: ${ctx.url} Query: ${ctx.request.querystring} `
-  );
+
+  console.log(`Method: ${ctx.method}  URL: ${ctx.url} `);
+  console.log(ctx.request);
 });
 
-// GET ?method=allTickets - список тикетов
 app.use(async (ctx) => {
-    if (ctx.request.query.method === "allTickets") {
-        ctx.body = JSON.stringify(allTickets);
-    }
-    if (ctx.request.query.method === "ticketById") {
-        const id = ctx.request.query.id;
-        const ticket = allTickets.find((ticket) => ticket.id == id);
-        console.log(id, ticket);
-        ctx.body = JSON.stringify(ticket);
-    }
+  // GET ?method=allTickets
+  if (ctx.request.query.method === "allTickets") {
+    ctx.body = JSON.stringify(allTickets);
+  }
+
+  // GET ?method=ticketById&id=<id>
+  if (ctx.request.query.method === "ticketById") {
+    const id = ctx.request.query.id;
+    const ticket = allTickets.find((ticket) => ticket.id == id);
+    console.log(id, ticket);
+    ctx.body = JSON.stringify(ticket);
+  }
+  
+  // POST ?method=createTicket - создание тикета (<id> генерируется на сервере, в теле формы name, description, status)
+  if (ctx.request.query.method === "createTicket") {
+    const { name, description, status } = ctx.request.body;
+    const id = uuid.v4();
+    const created = new Date().toISOString().slice(0, 10);
+    const ticket = { id, name, description, status, created };
+    allTickets.push(ticket);
+    ctx.body = JSON.stringify(ticket);
+  }
 });
-
-
-
-// POST ?method=createTicket - создание тикета (<id> генерируется на сервере, в теле формы name, description, status)
 
 app.listen(3000);
